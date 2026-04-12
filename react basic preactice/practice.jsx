f780e8112f7bf3e1db520fd9826c09c7aec6f5d3
sayak53 <sayakchakraborty928@gmail.com>
2026-04-12 14:53:31 +0530

    a react folder added just or practice purpose and completing task(task 2 added)

---
+++ react basic preactice/practice.jsx
@@ -0,0 +1,19 @@
+const Name = () => {
+  const name = "Sayak";
+
+  return (
+    <div>
+      <h1>Hello {name}</h1>
+    </div>
+  );
+};
+
+const Location = () => {
+  const city = "Kolkata";
+
+  return (
+    <div>
+      <h1>I Live in {city}</h1>
+    </div>
+  );
+};
--- test.py
+++ test.py
@@ -77,4 +77,4 @@
 
 
 #10.WAP to count the number of students with the “A” grade in the following tuple.
-# Store the above values in a list & sort them from “A” to “D”.[”C”,“D”,“A”,“A”,“B”,“B”,“A”]
+# Store the above values in a list & sort them from “A” to “D”.[”C”,“D”,“A”,“A”,“B”,“B”,“
