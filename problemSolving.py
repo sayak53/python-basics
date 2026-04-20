f8d677d85a54f180edbfb86d2ceae6a38dc7c50a
sayak53 <sayakchakraborty928@gmail.com>
2026-04-20 12:35:26 +0530

    solved daily leetcode problem

---
+++ problemSolving.py
@@ -0,0 +1,18 @@
+# from typing import List
+
+# class Solution:
+#     def maxDistance(self, colors: List[int]) -> int:
+#         n = len(colors)
+#         answer = 0
+
+#         for i in range(n):
+#             if colors[i] != colors[-1]:
+#                 answer = max(answer, n - 1 - i)
+#             if colors[i] != colors[0]:
+#                 answer = max(answer, i)
+
+#         return answer
+
+# if __name__ == "__main__":
+#     colors = [1, 1, 1, 6, 1, 1, 1]
+#     print(Solution().maxDistance(colors))
