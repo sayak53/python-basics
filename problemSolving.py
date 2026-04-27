e4afcd1627869c377be31ed90c4a73d66d7f3f1f
sayak53 <sayakchakraborty928@gmail.com>
2026-04-27 13:39:15 +0530

    revising old python problems

--- problemSolving.py
+++ problemSolving.py
@@ -1,18 +1,18 @@
-# from typing import List
+from typing import List
 
-# class Solution:
-#     def maxDistance(self, colors: List[int]) -> int:
-#         n = len(colors)
-#         answer = 0
+class Solution:
+    def maxDistance(self, colors: List[int]) -> int:
+        n = len(colors)
+        answer = 0
 
-#         for i in range(n):
-#             if colors[i] != colors[-1]:
-#                 answer = max(answer, n - 1 - i)
-#             if colors[i] != colors[0]:
-#                 answer = max(answer, i)
+        for i in range(n):
+            if colors[i] != colors[-1]:
+                answer = max(answer, n - 1 - i)
+            if colors[i] != colors[0]:
+                answer = max(answer, i)
 
-#         return answer
+        return answer
 
-# if __name__ == "__main__":
-#     colors = [1, 1, 1, 6, 1, 1, 1]
-#     print(Solution().maxDistance(colors))
+if __name__ == "__main__":
+    colors = [1, 1, 1, 6, 1, 1, 1]
+    print(Solution().maxDistance(colors))
