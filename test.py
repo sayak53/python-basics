8bc92297fa93e39f01da8f2dc3bd82abdea90e26
sayak53 <sayakchakraborty928@gmail.com>
2026-06-06 20:13:09 +0530

    one updated

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
-#1.Write a Program to input 2 numbers & print their sum.
-# num1=float(input('Enter 1st number:'))
-# num2=float(input('Enter 2nd number:'))
-# sum = num1+num2
-# print(f'the sum of {num1} and {num2} is {sum}')
+# 1.Write a Program to input 2 numbers & print their sum.
+num1=float(input('Enter 1st number:'))
+num2=float(input('Enter 2nd number:'))
+sum = num1+num2
+print(f'the sum of {num1} and {num2} is {sum}')
 
 
 #2.WAP to input 2 int numbers, a and b.
